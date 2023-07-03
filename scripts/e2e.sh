64c584103d1f1d7bfe32bcc25d6f12ddfc97c8ff
TechDufus <matthewjdegarmo@gmail.com>
2023-07-03 11:06:58 -0500

    adding e2e script

---
+++ scripts/e2e.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Get the current directory (so this script can be run from anywhere)
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+. $DIR/build.sh
+. $DIR/push.sh
